18f8fa835efd5f067ae2f864478532bf49db2518
makarimachmad <makarimachmad@gmail.com>
2017-11-18 14:04:20 +0700

    [makarim] tambah constructor

--- src/iter/view/LoginView.java
+++ src/iter/view/LoginView.java
@@ -1,5 +1,6 @@
 package iter.view;
 
+import java.awt.HeadlessException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,5 +10,9 @@ public class LoginView extends JFrame{
     private JLabel jlJudul, jlUser, jlPass;
     private JTextField jtUser, jtPass;
     private JButton jbLogin, jbRegister;
+
+    public LoginView() throws HeadlessException {
+    }
     
+   
 }
