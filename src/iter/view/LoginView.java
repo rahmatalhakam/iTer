5fb4ee2c341ac05c5b1cbb74e6b37ee6f3eee96a
Rahmat Al Hakam <rahmatalhakam@gmail.com>
2017-11-18 13:48:52 +0700

    [RAHMAT] add package view

---
+++ src/iter/view/LoginView.java
@@ -0,0 +1,13 @@
+package iter.view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class LoginView extends JFrame{
+    private JLabel jlJudul, jlUser, jlPass;
+    private JTextField jtUser, jtPass;
+    private JButton jbLogin, jbRegister;
+    
+}
